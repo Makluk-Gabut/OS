66cd1944fe96967467119f8838d95faadc1a1470
Bob/MaklukGabut <raylanwidjaya@gmail.com>
2026-08-06 19:36:35 +0900

    Delete GabutOS-v0.5.0/GabutOS directory

--- GabutOS-v0.5.0/GabutOS/tss.h
+++
@@ -1,9 +0,0 @@
-#ifndef TSS_H
-#define TSS_H
-
-#include <stdint.h>
-
-void tss_install(uint16_t ss0, uint32_t esp0);
-void tss_set_kernel_stack(uint32_t esp0);
-
-#endif
--- GabutOS-v0.5.0/GabutOS/usermode.h
+++
@@ -1,7 +0,0 @@
-#ifndef USERMODE_H
-#define USERMODE_H
-
-void usermode_install(void);
-void usermode_run_demo(void);
-
-#endif
