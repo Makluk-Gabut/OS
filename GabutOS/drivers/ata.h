9e24ec314e05379de0ac1ab804aaa0e8d6ad7566
MaklukGabut <raylanwidjaya@gmail.com>
2026-08-13 20:19:33 +0900

    Delete GabutOS directory for a greater version i hope :]

--- GabutOS/drivers/ata.h
+++
@@ -1,9 +0,0 @@
-#ifndef ATA_H
-#define ATA_H
-
-#include <stdint.h>
-
-int ata_read_sector(uint32_t lba, uint8_t* buffer);
-int ata_write_sector(uint32_t lba, const uint8_t* buffer);
-
-#endif
--- GabutOS/loader/elf.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELF_H
-#define ELF_H
-
-#include <stdint.h>
-
-int elf_load(const uint8_t* data, uint32_t size, uint32_t* out_entry, int ring, uint32_t* page_directory, uint32_t max_pages);
-
-#endif
